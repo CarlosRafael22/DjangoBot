4ada2f26fda1d4aa8e0fb9c71ef91f29fc53cd15
CarlosRafael22 <carlosrafael093@gmail.com>
2017-03-31 19:28:44 -0300

    Adicionado o codigo de criar csv. Tem que terminar o de peso que vai ser mais complicado de pegar as datas

--- bot/views.py
+++ bot/views.py
@@ -31,4 +31,31 @@ def export_csv_refeicao(request, participanteName):
             smart_str(obj.data.strftime("%Y-%m-%d %H:%M:%S")),
         ])
     return response
+
+ def export_csv_peso(request, participanteName):
+    import csv
+    from django.utils.encoding import smart_str
+
+    # import pdb;
+    # pdb.set_trace();
+
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = 'attachment; filename='+participanteName+'.csv'
+    writer = csv.writer(response, csv.excel)
+    response.write(u'\ufeff'.encode('utf8')) # BOM (optional...Excel needs it to open UTF-8 file properly)
+    writer.writerow([
+        smart_str(u"Peso"),
+        smart_str(u"Data"),
+    ])
+
+    # Fazendo a query para pegar as informacoes da refeicao do participante
+    participante = Participante.objects.get(nome=participanteName)
+    log_peso = Log_Peso.objects.filter(participante=participante)
+    for obj in refeicoes:
+        writer.writerow([
+            smart_str(obj.refeicao_nome),
+            smart_str(obj.descricao_refeicao),
+            smart_str(obj.data.strftime("%Y-%m-%d %H:%M:%S")),
+        ])
+    return response
 # export_csv.short_description = u"Export CSV"
